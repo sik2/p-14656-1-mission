7df50f1e584d111941ea4a2e9b89e680dc6e83f4
sik2 <bickwang123@gmail.com>
2026-01-05 16:32:07 +0900

    0012

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -19,11 +19,13 @@ public class BaseInitData {
     public ApplicationRunner baseInitDataRunner (){
         return args->{
             work1();
+            work2();
         };
     }
 
     private void work1() {
         log.debug("Post entity 개수: {}",postService.count());
+        log.debug("샘플 Post 데이터 생성");
         if (postService.count() == 0){
             for (int i = 1; i <= 10; i++) {
                 String title = "Sample Post Title " + i;
@@ -32,6 +34,13 @@ public class BaseInitData {
                 Post post = postService.create(title, content, author);
                 log.debug("Created Post: {}", post);
             }
+        }
+    }
+
+    private void work2(){
+        log.debug("기존 Post 전체 조회");
+        for (Post post : postService.findAll()) {
+            log.debug("Existing Post: {}", post);
         }
     }
 }
