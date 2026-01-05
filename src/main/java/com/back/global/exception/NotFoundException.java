c3959f15d32de021e8ed8bdc9f65f9835a83b385
sik2 <bickwang123@gmail.com>
2026-01-05 16:47:31 +0900

    0014

---
+++ src/main/java/com/back/global/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.back.global.exception;
+
+public class NotFoundException extends DomainException{
+    public NotFoundException(String resultCode, String message) {
+        super("404", message);
+    }
+}
+
