2765ae4895046d5210510557e0ac54f31d402f9a
JH Hwang <practice2280@gmail.com>
2026-01-05 17:03:31 +0900

    0015

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -48,8 +48,8 @@ public class BaseInitData {
     private void work3(){
         log.debug("Post 단건 조회");
         for (Post post : postService.findAll()) {
-            Post postRow = postService.findById(post.getId()).get();
-            log.debug("조회된 Post: {}", postRow);
+            Post fetchedPost = postService.findById(post.getId());
+            log.debug("조회된 Post: {}", fetchedPost);
         }
     }
 }
