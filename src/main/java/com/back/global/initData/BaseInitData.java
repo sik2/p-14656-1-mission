20ea77c1f53fed4742073e95c514213b90805030
김혜림 <khrim981209@gmail.com>
2026-01-11 04:17:53 +0900

    0008

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -1,5 +1,6 @@
 package com.back.global.initData;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
